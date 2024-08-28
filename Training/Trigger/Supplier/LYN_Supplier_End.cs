249d5e48d75db2f495c6aaf0f67f2841ff360f6a
xuanvinh223 <phamxuanvinh020203@gmail.com>
2024-08-28 15:18:06 +0700

    28/08/2024

---
+++ Training/Trigger/Supplier/LYN_Supplier_End.cs
@@ -0,0 +1,29 @@
+﻿using Ede.Uof.WKF.ExternalUtility;
+using System;
+using Training.Supplier.UCO;
+
+namespace Training.Trigger.Supplier
+{
+    public class LYN_Supplier_End : ICallbackTriggerPlugin
+    {
+        public void Finally()
+        {
+            //  throw new NotImplementedException();
+        }
+
+        public string GetFormResult(ApplyTask applyTask)
+        {
+     
+            SupplierUCO uco = new SupplierUCO();
+            string RNO = applyTask.FormNumber;
+            string signStatus = applyTask.FormResult.ToString();
+            uco.UpdateFormResult(RNO, signStatus);
+            return "";
+        }
+
+        public void OnError(Exception errorException)
+        {
+            //  throw new NotImplementedException();
+        }
+    }
+}
